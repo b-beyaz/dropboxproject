e974886b8ea9d30ef3087bb31c3495ea918707ff
aysebeyaz <ayse.beyaz@e-commint.com>
2025-05-06 00:09:42 +0300

    Initial commit

---
+++ src/main/java/com/example/dropboxproject/DropboxprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dropboxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropboxprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropboxprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dropboxproject/DropboxprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dropboxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropboxprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
